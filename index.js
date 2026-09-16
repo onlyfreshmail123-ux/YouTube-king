c352ec609cdf493654504777d024dfaefe5560fa
onlyfreshmail123-ux <onlyfreshmail123@gmail.com>
2026-09-16 08:19:48 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require("express");
-const cors = require("cors");
 const fs = require("fs");
 const path = require("path");
 const { spawn } = require("child_process");
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
+
+// CORS without external package
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
 
 const PORT = process.env.PORT || 3000;
 
